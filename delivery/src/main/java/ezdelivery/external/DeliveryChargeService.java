011231999a075f64a7054488b3b6e5102af03212
yoonmira <84304041+yoonmira@users.noreply.github.com>
2021-07-08 21:22:35 +0900

    Update DeliveryChargeService.java

--- delivery/src/main/java/ezdelivery/external/DeliveryChargeService.java
+++ delivery/src/main/java/ezdelivery/external/DeliveryChargeService.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
 
-@FeignClient(name="DeliveryCharge", url="http://localhost:8088")
+//@FeignClient(name="DeliveryCharge", url="http://localhost:8088")
+@FeignClient(name="DeliveryCharge", url="${api.url.DeliveryCharge}")
 public interface DeliveryChargeService {
 
     @RequestMapping(method= RequestMethod.GET, path="/deliveryCharges")
