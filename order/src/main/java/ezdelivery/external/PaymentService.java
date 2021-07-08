3c739651597a53961cd3dd562ad797b1b6f9a5ad
yoonmira <84304041+yoonmira@users.noreply.github.com>
2021-07-08 21:26:44 +0900

    Update PaymentService.java

--- order/src/main/java/ezdelivery/external/PaymentService.java
+++ order/src/main/java/ezdelivery/external/PaymentService.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 //api.url.payment ==> http://localhost:8083
-@FeignClient(name="payment", url="http://localhost:8083")
-//@FeignClient(name="payment", url="${api.url.payment}")
+//@FeignClient(name="payment", url="http://localhost:8083")
+@FeignClient(name="payment", url="${api.url.payment}")
 public interface PaymentService {
 
     @RequestMapping(method= RequestMethod.GET, path="/payments")
